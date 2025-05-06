6b11fc18f69efb6c7c590767731344f5a18bffe2
faviconico1910 <23521179@gm.uit.edu.vn>
2025-05-06 10:18:50 +0700

    add bai1

---
+++ Lab4/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtWebName = new TextBox();
+            btnGet = new Button();
+            txtContent = new RichTextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(103, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Tên trang web";
+            // 
+            // txtWebName
+            // 
+            txtWebName.Location = new Point(12, 32);
+            txtWebName.Name = "txtWebName";
+            txtWebName.Size = new Size(558, 27);
+            txtWebName.TabIndex = 1;
+            // 
+            // btnGet
+            // 
+            btnGet.Location = new Point(675, 12);
+            btnGet.Name = "btnGet";
+            btnGet.Size = new Size(126, 81);
+            btnGet.TabIndex = 2;
+            btnGet.Text = "GET";
+            btnGet.UseVisualStyleBackColor = true;
+            btnGet.Click += btnGet_Click;
+            // 
+            // txtContent
+            // 
+            txtContent.Location = new Point(12, 102);
+            txtContent.Name = "txtContent";
+            txtContent.Size = new Size(1188, 555);
+            txtContent.TabIndex = 3;
+            txtContent.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1212, 669);
+            Controls.Add(txtContent);
+            Controls.Add(btnGet);
+            Controls.Add(txtWebName);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtWebName;
+        private Button btnGet;
+        private RichTextBox txtContent;
+    }
+}
---
+++ Lab4/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(txtWebName.Text))
+                {
+                    MessageBox.Show("Vui lòng nhập trên trang web");
+                    return;
+                }
+                string content = getHTML(txtWebName.Text);
+                txtContent.AppendText(content);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            } 
+            
+
+
+        }
+        private string getHTML(string szUrl)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(szUrl);
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+                    "(KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36";
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            string html = "";
+            using (StreamReader sr = new StreamReader(response.GetResponseStream()))
+            {
+                html = sr.ReadToEnd();
+            } 
+                
+            return html;
+        }
+    }
+}
