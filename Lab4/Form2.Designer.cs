498426e2a596217fb4a740ad0690d542f2148eb1
hdevelovyou <buihieu195b@gmail.com>
2025-05-19 22:48:46 +0700

    bai 2

---
+++ Lab4/Form2.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Lab4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            btnPost = new Button();
+            inputTextBox = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(51, 29);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(570, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(51, 95);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(570, 326);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // btnPost
+            // 
+            btnPost.Location = new Point(643, 29);
+            btnPost.Name = "btnPost";
+            btnPost.Size = new Size(122, 27);
+            btnPost.TabIndex = 2;
+            btnPost.Text = "POST";
+            btnPost.UseVisualStyleBackColor = true;
+            btnPost.Click += btnPost_Click;
+            // 
+            // inputTextBox
+            // 
+            inputTextBox.Location = new Point(51, 62);
+            inputTextBox.Name = "inputTextBox";
+            inputTextBox.Size = new Size(570, 27);
+            inputTextBox.TabIndex = 3;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(inputTextBox);
+            Controls.Add(btnPost);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Button btnPost;
+        private TextBox inputTextBox;
+    }
+}
---
+++ Lab4/Form2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPost_Click(object sender, EventArgs e)
+        {
+            string url = textBox1.Text.Trim();
+            string postData = Uri.UnescapeDataString(inputTextBox.Text);
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = byteArray.Length;
+
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string responseText = reader.ReadToEnd();
+                        richTextBox1.AppendText(responseText);
+                    }
+                    response.Close();
+                }
+            }
+            catch (WebException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
